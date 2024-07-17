6334b1218750a512919dfd4d14381f409852faaf
SnehaaSah <sneha4567.sah@gmail.com>
2024-07-17 11:59:14 +0530

    user input and loops

--- dayone/src/comp/dayone/task1/TNSIF_practice.java
+++ dayone/src/comp/dayone/task1/TNSIF_practice.java
@@ -1,9 +1,48 @@
 package comp.dayone.task1;
+import java.util.Scanner;
 
 public class TNSIF_practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sc =  new Scanner(System.in);
+//		
+//		int s = sc.nextInt();
+//		String r = sc.nextLine();
+//		
+//		System.out.println("the input is:"+s);
+//		System.out.println("the input is:"+r);
+		
+		
+		// Loops
+		// 1. for loop
+		
+//		for (int l = 10; l >= 1;l-- ) {
+//			System.out.println("2 * "+ l + "="+ 2*l);
+//		}
+//		
+//		System.out.println("the forward table :");
+//		
+//		
+//		
+//		for (int l = 1; l <= 10;l++ ) {
+//			System.out.println("2 * "+ l + "="+ 2*l);
+//		}
+
+		// 2. While loop
+//		int a = 20;
+//		while (a <= 100) {
+//			System.out.println(a);
+//			a++;
+//		}
+		
+		//3 . Do While Loop
+		
+		int v = 30;
+		do {
+			System.out.println(v);
+			v++;
+		} while(v<=200);
+		
 
 	}
 
